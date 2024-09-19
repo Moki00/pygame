de02cf03488458d32f1c22959ca422f25c1e1005
King <Morgan.King@gwinnettcounty.com>
2024-09-19 12:38:36 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+import time
+import random
+
+WIDTH, HEIGHT = 1000, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Avoid Rain")
+
+BG = pygame.image.load("bg.jpeg")
+
+def draw():
+    WIN.blit(BG, (0,0))
+    pygame.display.update()
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
